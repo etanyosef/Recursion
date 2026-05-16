c8b6e131b0bdd50c02b50fc0c251de070b03a071
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-05-16 22:47:44 +0800

    solve merge sort

--- merge-sort.js
+++ merge-sort.js
@@ -1,6 +1,7 @@
-const mergeSort = (array) => {
+export const mergeSort = (array) => {
     if (array.length <= 1) return array;
 
+    // split the array
     const midOfArray = Math.floor(array.length / 2);
     let firstHalf = mergeSort(array.slice(0, midOfArray));
     let secondHalf = mergeSort(array.slice(midOfArray));
@@ -8,8 +9,16 @@ const mergeSort = (array) => {
     let sortedArray = [];
     
     while (firstHalf.length && secondHalf.length) {
+        // split the array and compare, then push it to sortedArray
         if (firstHalf[0] < secondHalf[0]) {
             sortedArray.push(firstHalf.shift());
+        } else {
+            sortedArray.push(secondHalf.shift());
         }
     }
+
+    return sortedArray.concat(firstHalf).concat(secondHalf);
 }
+
+console.log(mergeSort([]));
+
